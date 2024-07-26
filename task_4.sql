ac64ab718e08d5a2e38ef12550eb33b5f17ccdcd
DannyNak2 <danielnakachew1@gmail.com>
2024-07-26 22:05:57 +0300

    edited multiple lines of task 4 sql fiel

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,16 @@
+-- Select the database
 USE alx_book_store;
 
-SELECT * FROM books;
+-- Query to get full description of the table 'books'
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
